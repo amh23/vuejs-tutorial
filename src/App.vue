b42253ec00b6ecd526c4f673fa22bef8518df202
amh23 <ayemyahan23@gmail.com>
2023-07-01 14:46:01 +0630

    First Basic Vuejs Tutorial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <p>Hello from Vue</p>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            activePage: 0,
+            pages: [
+                {
+                    link: {text: 'Home',url: 'index.html'}, 
+                    pageTitle: 'HomePage',
+                    content: 'This is the home content'
+                },
+                {
+                    link: {text: 'About',url: 'about.html'}, 
+                    pageTitle: 'About Page',
+                    content: 'This is the about content'    
+                },
+                {
+                    link: {text: 'Contact',url: 'contact.html'}, 
+                    pageTitle: 'Contact Page',
+                    content: 'This is the contact content'
+                },
+            ]
+        };
+    }
+}
+</script>
