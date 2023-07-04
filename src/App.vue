6ecce32a02ea56a37a134db0c3ae12105958ef89
amh23 <ayemyahan23@gmail.com>
2023-07-04 22:53:58 +0630

    Navbar is created

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-    <p>Hello from Vue</p>
+    <navbar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+    <page-viewer
+        v-if="pages.length > 0"
+        :page="pages[activePage]"
+    ></page-viewer>
 </template>
 <script>
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue'
 export default{
+    components: {
+        Navbar,
+        PageViewer
+    },
+    created(){
+        this.getPages()
+    },
     data(){
         return{
             activePage: 0,
-            pages: [
-                {
-                    link: {text: 'Home',url: 'index.html'}, 
-                    pageTitle: 'HomePage',
-                    content: 'This is the home content'
-                },
-                {
-                    link: {text: 'About',url: 'about.html'}, 
-                    pageTitle: 'About Page',
-                    content: 'This is the about content'    
-                },
-                {
-                    link: {text: 'Contact',url: 'contact.html'}, 
-                    pageTitle: 'Contact Page',
-                    content: 'This is the contact content'
-                },
-            ]
+            pages: []
         };
+    },
+    methods: {
+        async getPages() {
+            let res = await fetch('pages.json');
+            let data = await res.json();
+
+           this.pages  = data;
+        }
     }
 }
 </script>
