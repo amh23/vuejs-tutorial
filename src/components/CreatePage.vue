329d12bccc90d3dbb33c8f42c32fde711e549da6
amh23 <ayemyahan23@gmail.com>
2023-07-10 22:01:48 +0630

    CreatePage component is implemented

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,43 +1,85 @@
 <template>
-    <div class="container mb-3">
-        <form action="">
-            <div class="mb-3">
-                <label for="" class="form-label">
-                    Page Title
-                </label>
-                <input
-                    type="text"
-                    class="form-control"
-                    v-model="pageTitle"
-                />
+    <form action="" class="container ">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="container mb-3">
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Page Title
+                        </label>
+                        <input type="text" class="form-control" v-model="pageTitle" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Content
+                        </label>
+                        <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
+                    </div>
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="" class="form-label">
+                                Link Text
+                            </label>
+                            <input type="text" class="form-control" v-model="linkText"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">
+                                Link URL
+                            </label>
+                            <input type="text" class="form-control" v-model="linkUrl"/>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <label class="form-check-label" for="gridCheck1">
+                                    Published
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <button
+                         class="btn btn-primary" 
+                        :disabled="isFormInValid"
+                         @click.prevent="submitForm">Create
+                        Page</button>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="" class="form-label">
-                    Content
-                </label>
-                <textarea
-                    type="text"
-                    class="form-control"
-                    rows="5"
-                    v-model="content"
-                ></textarea>
-            </div>
-            <div class="mb-3">
-                <button 
-                    class="btn btn-primary"
-                    @click.prevent="pageCreated({pageTitle, content})"
-                    >Create Page</button>
-            </div>
-        </form>
-    </div>
+        </div>
+    </form>
 </template>
 <script>
-export default{
+export default {
     props: ['pageCreated'],
-    data(){
+    computed: {
+        isFormInValid(){
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+        }
+    },
+    data() {
         return {
             pageTitle: '',
-            content: ''
+            content: '',
+            linkText: '',
+            linkUrl: ''
+        }
+    },
+    methods: {
+        submitForm(){
+            if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                alert('Please fill the form.');
+                return;
+            }
+
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link: {
+                    text: this.linkText,
+                    url: this.linkUrl
+                }
+            });
         }
     }
 }
