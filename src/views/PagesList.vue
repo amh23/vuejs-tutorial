a815e9cca0e8fa1a433c3a55bfafe473acce4394
amh23 <ayemyahan23@gmail.com>
2023-07-15 21:01:06 +0630

    using reactive

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,14 +1,17 @@
 <template>
-    <p>{{ counter }}</p>
+    <p>{{ data.counter }}</p>
     <button @click.prevent="click">Click</button>
 </template>
 <script setup>
 import { ref, reactive } from 'vue';
 
-const counter = ref(0); // {value: 0}
+//const counter = ref(0); // {value: 0}
+
+const data = reactive({counter: 0});
 
 function click(){
-    counter.value++;
+    //counter.value++;
+    data.counter++;
 }
 // export default{
 //     data(){
