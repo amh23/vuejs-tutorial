2f44861e4647133694f9275cb8f28f074afa257c
amh23 <ayemyahan23@gmail.com>
2023-07-15 14:27:14 +0630

    A component respond to the route parameter

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="container">
-        <h1 class="emphasize">Page Title</h1>
-        <p>Content</p>
+    <div v-if="page" class="container">
+        <h1 class="emphasize">{{ page.pageTitle}}</h1>
+        <p>{{ page.content }}</p>
     </div>
 </template>
 <script>
 export default{
-    props: {
-        page: {
-            type: Object,
-            default(rawProps){
-                return {
-                    pageTitle: '',
-                    content: ''
-                };
-            }
-        }
-    }
+   created(){
+        this.page = this.$pages.getSinglePage(this.$route.params.index);
+
+        this.$watch(() => this.$route.params, (newParams, oldParams) =>{
+                                                    this.page = this.$pages.getSinglePage(newParams.index);
+                                                 });
+   },
+   data(){
+        return {
+            page: null
+        };
+   }
 }
 </script>
 <style scoped>
