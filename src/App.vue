29c9c7b5c11ed933816cd548800c2f15f54eee65
amh23 <ayemyahan23@gmail.com>
2023-07-15 13:48:18 +0630

    Routes are created

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
         :pages="pages"
         :active-page="activePage"
     ></navbar>
-    <page-viewer
+    <router-view></router-view>
+    <!-- <page-viewer
         v-if="pages.length > 0"
         :page="pages[activePage]"
     ></page-viewer>
     <create-page
         @page-created="pageCreated"
-    ></create-page>
+    ></create-page> -->
 </template>
 <script>
 import PageViewer from './components/PageViewer.vue';
