36b51dc87de6f14bb7edc4d54e3033b2de6c1e2a
amh23 <ayemyahan23@gmail.com>
2023-07-15 20:58:01 +0630

    using ref

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,25 @@
 <template>
-    <p>This is the PagesList</p>
+    <p>{{ counter }}</p>
+    <button @click.prevent="click">Click</button>
 </template>
+<script setup>
+import { ref, reactive } from 'vue';
+
+const counter = ref(0); // {value: 0}
+
+function click(){
+    counter.value++;
+}
+// export default{
+//     data(){
+//         return {
+//             counter: 0
+//         };
+//     },
+//     methods: {
+//         click(){
+//             this.counter++;
+//         }
+//     }
+// }
+</script>
